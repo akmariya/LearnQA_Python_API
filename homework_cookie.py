51b9f5ab1df4af85a00deb16337cca87efd75322
Mari <ak_mariya@tut.by>
2021-11-06 16:23:42 +0300

    Ex11

--- homework_cookie.py
+++ homework_cookie.py
@@ -7,7 +7,7 @@ def test_cookie():
     actual_cookie_name = response.headers.get("Set-cookie")
     expect_cookie_name = "HomeWork"
     assert expect_cookie_name in actual_cookie_name, "Неправильное название cookie"
-    
+
     actual_result = response.cookies.get("HomeWork")
     expected_result = "hw_value"
     assert expected_result in actual_result, "Отдается неправильная cookie"
