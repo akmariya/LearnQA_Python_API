d67409fbd41d9bf2eeed0472c2dd3cf8b34e0094
Mari <ak_mariya@tut.by>
2021-11-06 16:21:10 +0300

    Ex10

--- homework_cookie.py
+++ homework_cookie.py
@@ -3,6 +3,11 @@ import requests
 
 def test_cookie():
     response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
+
+    actual_cookie_name = response.headers.get("Set-cookie")
+    expect_cookie_name = "HomeWork"
+    assert expect_cookie_name in actual_cookie_name, "Неправильное название cookie"
+    
     actual_result = response.cookies.get("HomeWork")
     expected_result = "hw_value"
     assert expected_result in actual_result, "Отдается неправильная cookie"
